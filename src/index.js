f1c9c36cdb0e9ec43bd5b89e70f492e3bfd424fa
dmytrobin <dmytro.binyk@gmail.com>
2022-01-05 14:43:04 +0200

    init: create basic structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
   
-}
+    let result, numToString, len, arr,arrReverse;
+   /* numToString = n.toString();
+    len = numToString.length;
+    arr =numToString.split('');
+    arrReverse = arr.reverse();
+    result = arrReverse.join('')*1;
+    console.log(arrReverse);
+    */
+    return result;
+   }
+   
+   //let result;
+   //let num = 12423534;
+   
+   //result = reverse(num);
+   //console.log(result);
