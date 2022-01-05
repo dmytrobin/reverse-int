b7e449af5a278f4c4eb03488a70c52506e9b0ffd
dmytrobin <dmytro.binyk@gmail.com>
2022-01-05 15:02:29 +0200

    feat: complete code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 module.exports = function reverse (n) {
-  
-    let result, numToString, len, arr,arrReverse;
-   /* numToString = n.toString();
-    len = numToString.length;
+    let result, numToString, len, arr, arrReverse;
+    n = Math.abs(n);
+    numToString = n.toString();
+   // len = numToString.length;
+
     arr =numToString.split('');
     arrReverse = arr.reverse();
     result = arrReverse.join('')*1;
-    console.log(arrReverse);
-    */
+   // console.log(arrReverse);
+    
     return result;
    }
    
-   //let result;
-   //let num = 12423534;
+   let result;
+   let num = 12423534;
    
    //result = reverse(num);
-   //console.log(result);
+   console.log(result);
